54a7119f8e6762880a029937f1ba40ab9621083e
Philip <philipmaheer@gmail.com>
2024-07-14 13:30:23 +0300

    add some style and modifiy mentors section

--- src/compounds/2-mentors/Mentors.jsx
+++ src/compounds/2-mentors/Mentors.jsx
@@ -1,17 +1,34 @@
 import "./mentors.css"
 import Card from "./card/Card"
-
+// @ts-ignore
+import database from "/src/tech/database.png"
+// @ts-ignore
+import html from "/src/tech/html.png"
+// @ts-ignore
+import css from "/src/tech/css.png"
+// @ts-ignore
+import js from "/src/tech/JS.png"
+// @ts-ignore
+import react from "/src/tech/react.png"
+// @ts-ignore
+import Redux from "/src/tech/Redux.png"
+// @ts-ignore
+import git from "/src/tech/git.png"
+// @ts-ignore
+import github from "/src/tech/github.png"
+// @ts-ignore
+import oop from "/src/tech/oop.png"
 
 function Mentors() {
     const myMentors = [
-        { id: "1", name: "Eng.Osama ElZero", image: "../../../elzero.cb9596212972463431ba.jpg", info: "I would like to express my special thanks to my first mentor, Eng. Osama, who introduced me to the world of programming.", tech: ["", "CSS", "JS", "git" , "github"], channal: "https://www.youtube.com/@ElzeroWebSchool" },
-        { id: "2", name: "Eng.Ahmed Nasser", image: "../../../codeZone2.f4faf4ab609cf11cac4a.png", info: "oooooooooooooooooooooooooooooooo", tech: ["Reactjs", "Redux",], channal: "https://www.youtube.com/@codeZone" },
-        { id: "4", name: "Eng.Mohammed ElDsoke", image: "../../../dsouke.96415ef3d1dc0a78e0bf.jpg", info: "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn", tech: ["OOP"], channal: "" },
-        { id: "3", name: "Eng.Salma Mamdouh", image: "../../../salma.png", info: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", tech: ["Database"], channal: "https://maharatech.gov.eg/course/view.php?id=740" },
+        { id: "1", name: "Eng.Osama ElZero", image: "../../../elzero.cb9596212972463431ba.jpg", info: "I would like to express my special thanks to my first mentor, Eng. Osama, who introduced me to the world of programming.", tech: [html, css, js, git , github], channal: "https://www.youtube.com/@ElzeroWebSchool" },
+        { id: "2", name: "Eng.Ahmed Nasser", image: "../../../codeZone2.f4faf4ab609cf11cac4a.png", info: "oooooooooooooooooooooooooooooooo", tech: [react, Redux,], channal: "https://www.youtube.com/@codeZone" },
+        { id: "4", name: "Eng.Mohammed ElDsoke", image: "../../../dsouke.96415ef3d1dc0a78e0bf.jpg", info: "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn", tech: [oop], channal: "https://www.youtube.com/@DesoukiEgypt" },
+        { id: "3", name: "Eng.Salma Mamdouh", image: "../../../salma.png", info: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", tech: [database], channal: "https://maharatech.gov.eg/course/view.php?id=740" },
     ]
     return (
         <>
-            <Card item={myMentors}/>
+            <Card item={myMentors} />
         </>
     )
 }
--- src/compounds/2-mentors/card/Card.jsx
+++ src/compounds/2-mentors/card/Card.jsx
@@ -1,6 +1,12 @@
 import { useEffect } from 'react'
 import './card.css'
 import { useRef } from 'react'
+// @ts-ignore
+import oop from "/src/tech/oop.png"
+import Swal from 'sweetalert2'
+import { color, delay, distance, easeOut, inView } from 'framer-motion'
+
+
 function Card(props) {
     const mentorSection = useRef()
     const headerSection = useRef()
@@ -8,27 +14,59 @@ function Card(props) {
     console.log(mentorSection.current);
     
     const handelscrolling = ()=>{
+        // @ts-ignore
         if(window.scrollY >= mentorSection.current.offsetTop && window.scrollY <= mentorSection.current.offset ){
+            // @ts-ignore
             headerSection.current.style.letterSpacing = "54px"
         }
         else{
+            // @ts-ignore
             headerSection.current.style.letterSpacing = "15px"
         }
     }
     useEffect(() => {
         window.addEventListener("scroll", handelscrolling)
-
     },[])
+    const handelMentor = (Mentors , i)=>{
+        console.log(Mentors.channal);
+        Swal.fire({
+            html: `You can use <b>bold</b>, <i>italic</i>, and <a href=${Mentors[i].channal}>links</a> here!`
+        });
+        // Swal.fire({
+        //     // popup: 'swal2-show',
+        //     // backdrop: 'swal2-backdrop-show',
+        //     // icon: 'swal2-icon-show',
+        //     width : "60%" ,
+        //     padding : "20px",
+        //     animation : true , 
+        //     grow : "column" , 
+        //     title: `<h5>${Mentors[i].name}<h5/>`,
+        //     color : "blue",
+        //     text: Mentors[i].info,
+        //     imageUrl: Mentors[i].image,
+        //     imageWidth: 250,
+        //     imageHeight: 250,
+        //     imageAlt: Mentors[i].name ,
+        //     showConfirmButton : false , 
+        //     footer: `<div className = "flex g-20">
+        //                 <a href= ${Mentors[i].channal} style = ${{color : "red"}}>YouTube</a>
+        //                 <a href= ${Mentors[i].channal} style = ${{color : "red"}}>Website</a>
+        //                 <a href= ${Mentors[i].channal} style = ${{color : "red"}}>linkedin</a>
+        //                 <a href= ${Mentors[i].channal} style = ${{color : "red"}}>facebook</a>
+        //                 <swal-button>
+        //             </div>`
+        // });
+    }
     // eslint-disable-next-line react/prop-types
     const Mentors = props.item
     // eslint-disable-next-line react/prop-types
-    const ListOfMentors = Mentors.map((item) =>
+    const ListOfMentors = Mentors.map((item , i) =>
         <>
-            <div className="mentor-card" key={item.id}>
+            <div className="mentor-card" key={item.id} onClick={()=>handelMentor(Mentors , i)}>
                 <div className='image-holder'>
                     <img className='card-image' src={item.image} alt="" />
-                    <div className='mentor-tech'>{item.tech.map((ele)=>{
-                            return <p className='tech'>{ele}</p>      
+                    <div className='mentor-tech'>{item.tech.map((ele , index)=>{
+                            return <img src= {ele} alt="" key={index} className={ele === oop ? 'tech-image oop' : 'tech-image'}/>     
                     })}</div>
                 </div>
                 <h2 className='mentor-name'>{item.name}</h2>
